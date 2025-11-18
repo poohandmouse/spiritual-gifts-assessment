5e8fc685db80b989eae527381a937470f1597f6a
poohandmouse <wshanehopkins@gmail.com>
2025-11-17 22:15:07 -0600

    Update config.js with loading instructions
    Add instructions to load custom configuration before Firebase SDKs.

---
+++ config.js
@@ -0,0 +1,8 @@
+<!-- Load Firebase SDKs -->
+<script type="module">
+
+**Add the following line just before the `<!-- Load Firebase SDKs -->` comment:**
+
+```html
+<!-- Load custom configuration first -->
+<script src="./config.js"></script>
